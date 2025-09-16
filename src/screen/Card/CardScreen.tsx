5342befefa21e68526216c9d747b0daffa440171
rommel-nazaryan <rommel@predipie.com>
2025-09-16 11:01:06 +0400

    Cartempty is done

--- src/screen/Card/CardScreen.tsx
+++ src/screen/Card/CardScreen.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
-
+import { StyleComponent } from '../../utiles/styles'
+import { Color } from '../../utiles/color'
+import ShoppingCard from '../../assets/svg/ShoppingCart.svg'
+import HorizontalFlatList from '../../component/HorizontalFlatList'
 export default function CardScreen() {
+  const {Styles} = StyleComponent();
   return (
-    <View>
-      <Text>CardScreen</Text>
+    <View style={Styles.container}>
+      <ScrollView>
+      <View style={[Styles.alignCenter,Styles.alignSelf,{width:'93%',marginTop:'12%'}]}>
+      <ShoppingCard fill={Color.black}/>
+      <Text style={Styles.h3_Bold}>Your Cart is Empty</Text>
+      <Text style={[Styles.h6_Regular,Styles.textAlign,{width:'80%'}]}>Once you add items from a store,
+      your cart will appear here</Text>
+      </View>
+      <View style={[Styles.alignSelf,{width:'93%',marginTop:'8%'}]}>
+      <Text style={[Styles.h3_Bold,{marginLeft:'2%'}]}>You might also like</Text>
+      <HorizontalFlatList />
+      </View>
+      </ScrollView>
     </View>
   )
 }
